43af42d58bad8e2b8017c49957dcab1ec19c9f5c
ShubhamkarJos <shubhamkarjoshi@gmail.com>
2021-03-17 23:30:13 +0530

    Login Page

--- Login/server.js
+++ Login/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const path = require('path')
+const 	bodyParser = require('body-parser')
 
 const app = express()
 app.use('/', express.static(path.join(_dirname, 'static')))
+app.use(bodyParser.json())
+app.post('/api/register', async (req, res) => {
+	console.log(req.body)
+})
 
 app.listen(9999, () => {
 	console.log('Server up at 9999')
