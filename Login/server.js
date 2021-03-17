93d409e0744a53b141a2d77fd0378ad6389d72e4
ShubhamkarJos <shubhamkarjoshi@gmail.com>
2021-03-18 00:34:00 +0530

    Login Page

--- Login/server.js
+++ Login/server.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const path = require('path')
-const 	bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const User = require('./model/user')
+mongoose.connect('mongodb://locolhost:27017/login-app-db', {
+	useNewUrlParser:true, 
+	useUnifiedTopology: true,	
+	useCreateIndex: true
+}).then(() => console.log('MongoDB Connected...')).catch((err) => console.log(err))
+
+
 
 const app = express()
-app.use('/', express.static(path.join(_dirname, 'static')))
+app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
+
 app.post('/api/register', async (req, res) => {
-	console.log(req.body)
+	console.log(req.body())
+	res.json({status: 'ok'})
 })
 
 app.listen(9999, () => {
