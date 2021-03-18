0d61c3eadefceadd0754a7181961c31d4ac5b37b
ShubhamkarJos <shubhamkarjoshi@gmail.com>
2021-03-18 17:14:53 +0530

    Login check

--- Login/index.js
+++ Login/index.js
@@ -1,11 +1,56 @@
-const express = require('express');
-const app = express();
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
 
-//Import Routes
-const authRoute = require('./routes/auth');
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/mydb',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var phno = req.body.phno;
+    var password = req.body.password;
+
+    var data = {
+        "name": name,
+        "email" : email,
+        "phno": phno,
+        "password" : password
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('signup_success.html')
+
+})
 
 
-//Route Middlewares
-app.use('/api/user', authRoute);
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+}).listen(3000);
 
-app.listen(3000, () => console.log("Server Up and running!"));
+
+console.log("Listening on PORT 3000");
