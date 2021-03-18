45f98764333e3ace113282ce9cca1d8e8411154c
ShubhamkarJos <shubhamkarjoshi@gmail.com>
2021-03-18 14:42:16 +0530

    new login

--- Login/model/user.js
+++ Login/model/user.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose')
-const UserSchema = new mongoose.Schema({
-	username: {type: String, required: true, unique:true },
-	password: {type: String, required: true},
+const router = require('express').Router();
 
-	}, 
-	{collections: 'users'}
-)
+router.post('/register', (req, res) => {
+	res.send('Register');
+});
 
-const model = mongoose.model('UserSchema', UserSchema)
 
-module.exports = model
+
+
+
+
+
+module.exports = router;
--- Login/server.js
+++ Login/server.js
@@ -1,25 +1,4 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const User = require('./model/user')
-mongoose.connect('mongodb://locolhost:27017/login-app-db', {
-	useNewUrlParser:true, 
-	useUnifiedTopology: true,	
-	useCreateIndex: true
-}).then(() => console.log('MongoDB Connected...')).catch((err) => console.log(err))
+const express = require('express');
+const app = express();
 
-
-
-const app = express()
-app.use('/', express.static(path.join(__dirname, 'static')))
-app.use(bodyParser.json())
-
-app.post('/api/register', async (req, res) => {
-	console.log(req.body())
-	res.json({status: 'ok'})
-})
-
-app.listen(9999, () => {
-	console.log('Server up at 9999')
-})
+app.listen(3000, () => console.log("Server Up and running!"));
